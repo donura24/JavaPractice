d85ad4837e3f4575c391b798719a3b1870d4925d
TeodorPenchev <teodor.penchev@notolytix.com>
2023-01-05 21:05:32 +0200

    commit

---
+++ printf.java
@@ -0,0 +1,31 @@
+package JavaPractice;
+
+public class printf {
+    public static void main(String[] args) {
+
+        boolean bool = true;
+        char myChar= '$';
+        String name = "Teodor";
+        int num = 4;
+        double doubleD= 1000;
+/*
+        System.out.printf("%b",bool);
+        System.out.printf("%c",myChar);
+        System.out.printf("%s",name);
+        System.out.printf("%d",num);
+        System.out.printf("%f",doubleD);
+*/
+        //System.out.printf("My name is: %10s", name);
+        //System.out.printf("This is the number: %.2f",doubleD);
+
+        //System.out.printf("This is the number: %-20f",doubleD);
+        //System.out.printf("This is the number: %+.2f",doubleD);
+        //System.out.printf("This is the number: %020f",doubleD); //This is the number: 0000000000100.000000
+        //System.out.printf("This is the number: %,f",doubleD); //This is the number: 1,000.000000
+
+
+
+
+
+    }
+}
