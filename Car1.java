8a440e1a6435de1ddf9f1150f4e55e1d9c45b635
donura24 <teodorpenchev24@gmail.com>
2023-01-06 23:16:41 +0200

    commit

---
+++ Car1.java
@@ -0,0 +1,13 @@
+package JavaPractice;
+
+public class Car1 {
+
+    String make = "Subaru";
+    String model = "Impreza WRX STI";
+    int year = 2023;
+
+    public String toString(){
+        return make + "\n" + model + "\n" + year;
+    }
+
+}
---
+++ toString.java
@@ -0,0 +1,10 @@
+package JavaPractice;
+
+public class toString {
+    public static void main(String[] args) {
+
+        Car1 car1 = new Car1();
+        //System.out.println(car1.toString());
+        System.out.println(car1);
+    }
+}
