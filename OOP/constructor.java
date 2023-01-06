2ed1e82bd25cced15ab92ff151d85e0d3994f863
donura24 <teodorpenchev24@gmail.com>
2023-01-06 22:31:33 +0200

    commit

---
+++ OOP/constructor.java
@@ -0,0 +1,12 @@
+package JavaPractice.OOP;
+
+public class constructor {
+
+    public static void main(String[] args) {
+           Human human1 = new Human("Teo", 26, 94.0);
+           Human human2 = new Human("Bojidar", 30, 100.0);
+
+        System.out.println(human1.name);
+        System.out.println(human2.name);
+    }
+}
