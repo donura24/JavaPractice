e9682ac927a55046874189e6a995089589c59701
donura24 <teodorpenchev24@gmail.com>
2023-01-11 03:30:06 +0200

    commit

--- OOP/File_Class.java
+++ OOP/File_Class.java
@@ -5,10 +5,14 @@ import java.io.File;
 public class File_Class {
     public static void main(String[] args) {
 
-        File file = new File("/home/teodor.penchev/IdeaProjects/untitled/src/JavaPractice/Dog.java");
+        File file = new File("Dog.java");
 
         if (file.exists()){
             System.out.println("The file EXISTS!");
+            System.out.println(file.getPath());
+            System.out.println(file.getAbsolutePath());
+            System.out.println(file.isFile());
+            System.out.println(file.getTotalSpace());
         } else {
             System.out.println("The file is MISSING!");
         }
