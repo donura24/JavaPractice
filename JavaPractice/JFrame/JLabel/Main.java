46c604f5fb4040990cd898832cb42ab7e6cf2774
donura24 <teodorpenchev24@gmail.com>
2023-01-12 20:51:29 +0200

    commit

--- JavaPractice/JFrame/JLabel/Main.java
+++ JavaPractice/JFrame/JLabel/Main.java
@@ -1,29 +1,36 @@
 package JavaPractice.JavaPractice.JFrame.JLabel;
 
 import javax.swing.*;
+import javax.swing.border.Border;
 import java.awt.*;
 
 public class Main {
     public static void main(String[] args) {
 
-        ImageIcon icon = new ImageIcon("AvatarMaker.png");
-
+        ImageIcon icon = new ImageIcon("/home/teodor.penchev/Downloads/AvatarMaker.png");
+        Border border = BorderFactory.createLineBorder(new Color(200,50,120),5);
         JLabel label = new JLabel();
         label.setText("How are you?");
         label.setIcon(icon);
         label.setHorizontalTextPosition(JLabel.CENTER);
-        label.setVerticalTextPosition(JLabel.CENTER);
+        label.setVerticalTextPosition(JLabel.BOTTOM);
         label.setForeground(Color.GREEN);
         label.setBackground(new Color(30, 50, 150));
+        label.setOpaque(true); // display background color
         label.setFont(new Font("Italic", Font.BOLD,25));
-        label.setBounds(0,0, 250,250);
+        label.setIconTextGap(25);
+        //label.setBounds(0,0, 500,500);
+        label.setBorder(border);
+        label.setVerticalAlignment(JLabel.CENTER);
+        label.setHorizontalAlignment(JLabel.CENTER);
 
         JFrame jFrame = new JFrame();
-        jFrame.setSize(950,950);
+        //jFrame.setSize(950,950);
         jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //jFrame.setLayout(null);
         jFrame.add(label);
-        jFrame.setBackground(Color.DARK_GRAY);
         jFrame.setVisible(true);
+        jFrame.pack();
 
     }
 }
