efbff48f7fc40a70b552bb1592748dab9e12f698
donura24 <teodorpenchev24@gmail.com>
2023-01-21 20:49:36 +0200

    commit

---
+++ JavaPractice/JFrame/KeyBindings/Main.java
@@ -0,0 +1,17 @@
+package JavaPractice.JavaPractice.JFrame.KeyBindings;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // Key Bindings = Bind and Action to a KeyStroke
+        //                don't require you to click a component to give it focus
+        //                all Swing components use Key Bindings
+        //                increased flexibility compared to KeyListeners
+        //                can assign keystrokes to individual Swing components
+        //                more difficult to utilize and set up.
+
+
+        Game game = new Game();
+
+    }
+}
