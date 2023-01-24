d1b8f97505494831c4060707ebc152fd3190f1df
donura24 <teodorpenchev24@gmail.com>
2023-01-24 21:55:17 +0200

    commit

---
+++ Packages_/Main.java
@@ -0,0 +1,13 @@
+package JavaPractice.Packages_;
+
+import JavaPractice.Packages_.Toolbox.Toolbox;
+
+import javax.swing.*; // ctl + 'swing' click will lead us to the library.
+
+public class Main {
+    public static void main(String[] args) {
+
+        Toolbox toolbox = new Toolbox();
+        ImageIcon icon = new ImageIcon();
+    }
+}
---
+++ Packages_/Toolbox/Toolbox.java
@@ -0,0 +1,5 @@
+package JavaPractice.Packages_.Toolbox;
+
+public class Toolbox {
+
+}
