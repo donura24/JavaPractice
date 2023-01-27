8bfa22c3113d78e1e27ea06d76fa0c6a680e9e4f
donura24 <teodorpenchev24@gmail.com>
2023-01-27 18:12:35 +0200

    commit

--- JavaTextEditorApp/TextEditor.java
+++ JavaTextEditorApp/TextEditor.java
@@ -6,12 +6,23 @@ import javax.swing.event.ChangeListener;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 
 public class TextEditor extends JFrame implements ActionListener {
     JTextArea textArea;
     JScrollPane scrollPane;
     JLabel fontSizeLabel;
     JSpinner fontSizeSpinner;
+    JButton fontColorButton;
+    JComboBox fontBox;
+
+    JMenuBar menuBar;
+    JMenu fileMenu;
+    JMenuItem openItem;
+    JMenuItem saveItem;
+    JMenuItem exitItem;
 
     TextEditor() {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -42,15 +53,81 @@ public class TextEditor extends JFrame implements ActionListener {
             }
         });
 
+        fontColorButton = new JButton("Color");
+        fontColorButton.addActionListener(this);
 
+        String[] fonts = GraphicsEnvironment.getLocalGraphicsEnvironment().getAvailableFontFamilyNames();
+        fontBox = new JComboBox(fonts);
+        fontBox.addActionListener(this);
+        fontBox.setSelectedItem("Purisa");
+
+        // ---------- menubar --------
+        menuBar = new JMenuBar();
+        fileMenu = new JMenu("File");
+        openItem = new JMenuItem("Open");
+        saveItem = new JMenuItem("Save");
+        exitItem = new JMenuItem("Exit");
+
+        openItem.addActionListener(this);
+        saveItem.addActionListener(this);
+        exitItem.addActionListener(this);
+
+        fileMenu.add(openItem);
+        fileMenu.add(saveItem);
+        fileMenu.add(exitItem);
+        menuBar.add(fileMenu);
+
+        this.setJMenuBar(menuBar);
         this.add(fontSizeLabel);
         this.add(fontSizeSpinner);
+        this.add(fontColorButton);
+        this.add(fontBox);
         this.add(scrollPane);
         this.setVisible(true);
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
+
+        if (e.getSource() == fontColorButton) {
+            JColorChooser colorChooser = new JColorChooser();
+
+            Color color = JColorChooser.showDialog(null, "Choose a color", Color.black);
+            textArea.setForeground(color);
+        }
+        if (e.getSource() == fontBox) {
+
+            textArea.setFont(new Font((String) fontBox.getSelectedItem(), Font.PLAIN, textArea.getFont().getSize()));
+        }
+        if (e.getSource()==openItem){
+
+        }
+        if (e.getSource()==saveItem){
+            JFileChooser fileChooser = new JFileChooser();
+            fileChooser.setCurrentDirectory(new File("."));
+
+            int response = fileChooser.showSaveDialog(null);
+            if (response == JFileChooser.APPROVE_OPTION){
+                File file;
+                PrintWriter fileOut = null;
+
+                file = new File(fileChooser.getSelectedFile().getAbsolutePath());
+                try {
+                    fileOut = new PrintWriter(file);
+                    fileOut.println(textArea.getText());
+                } catch (FileNotFoundException ex) {
+                    throw new RuntimeException(ex);
+                }
+                finally {
+                    fileOut.close();
+                }
+            }
+
+        }
+        if (e.getSource()==exitItem){
+
+            System.exit(0);
+        }
 
     }
 }
