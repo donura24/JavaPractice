b1eef41e45556dbf389f5853203867968da33e61
donura24 <teodorpenchev24@gmail.com>
2023-01-31 19:20:57 +0200

    commit

--- JavaQuizGame/Quiz.java
+++ JavaQuizGame/Quiz.java
@@ -1,6 +1,7 @@
 package JavaPractice.JavaQuizGame;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -48,7 +49,61 @@ public class Quiz implements ActionListener {
     JTextField number_right = new JTextField();
     JTextField percentage = new JTextField();
     public Quiz() {
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(900,900);
+        frame.setLocationRelativeTo(null);
+        frame.getContentPane().setBackground(new Color(90,20,200));
+        frame.setLayout(null);
+        frame.setResizable(false);
 
+        textField.setBounds(0,0,900,50);
+        textField.setBackground(new Color(100,25,100));
+        textField.setForeground(Color.green);
+        textField.setFont(new Font("Ubuntu", Font.BOLD,25));
+        textField.setBorder(BorderFactory.createBevelBorder(1));
+        textField.setHorizontalAlignment(JTextField.CENTER);
+        textField.setEditable(false);
+
+        textArea.setBounds(0,50,900,50);
+        textArea.setLineWrap(true);
+        textArea.setWrapStyleWord(true);
+        textArea.setBackground(new Color(20,200,100));
+        textArea.setForeground(Color.green);
+        textArea.setFont(new Font("Ubuntu", Font.BOLD,20));
+        textArea.setBorder(BorderFactory.createBevelBorder(1));
+        textArea.setEditable(false);
+
+        buttonA.setBounds(0,100,100,100);
+        buttonA.setFont(new Font("Ubuntu",Font.BOLD,35));
+        buttonA.setFocusable(false);
+        buttonA.addActionListener(this);
+        buttonA.setText("A");
+
+        buttonB.setBounds(0,200,100,100);
+        buttonB.setFont(new Font("Ubuntu",Font.BOLD,35));
+        buttonB.setFocusable(false);
+        buttonB.addActionListener(this);
+        buttonB.setText("B");
+
+        buttonC.setBounds(0,300,100,100);
+        buttonC.setFont(new Font("Ubuntu",Font.BOLD,35));
+        buttonC.setFocusable(false);
+        buttonC.addActionListener(this);
+        buttonC.setText("C");
+
+        buttonD.setBounds(0,400,100,100);
+        buttonD.setFont(new Font("Ubuntu",Font.BOLD,35));
+        buttonD.setFocusable(false);
+        buttonD.addActionListener(this);
+        buttonA.setText("D");
+
+        frame.add(buttonA);
+        frame.add(buttonB);
+        frame.add(buttonC);
+        frame.add(buttonD);
+        frame.add(textArea);
+        frame.add(textField);
+        frame.setVisible(true);
     }
 
     public void nextQuestion() {
