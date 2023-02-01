e23989923d5221098c1d3e3f2c0a3b5c28fef276
donura24 <teodorpenchev24@gmail.com>
2023-02-01 21:01:27 +0200

    commit

--- JavaQuizGame/Quiz.java
+++ JavaQuizGame/Quiz.java
@@ -18,9 +18,8 @@ public class Quiz implements ActionListener {
             {"Ubuntu", "Arch", "BSD", "Slackware"}
     };
     char[] answers = {
-            'A',
+            'D',
             'B',
-            'C',
             'D'
     };
 
@@ -48,103 +47,119 @@ public class Quiz implements ActionListener {
     JLabel seconds_left = new JLabel();
     JTextField number_right = new JTextField();
     JTextField percentage = new JTextField();
+
+    Timer counter10 = new Timer(1000, new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+            seconds--;
+            seconds_left.setText(String.valueOf(seconds));
+            if (index==questions.length){
+                counter10.stop();
+            }
+            if (seconds <= 0 ) {
+                displayAnswer();
+            }
+        }
+    });
+
     public Quiz() {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(900,650);
+        frame.setSize(900, 650);
         frame.setLocationRelativeTo(null);
-        frame.getContentPane().setBackground(new Color(90,20,200));
+        frame.getContentPane().setBackground(new Color(90, 20, 200));
         frame.setLayout(null);
         frame.setResizable(false);
 
-        textField.setBounds(0,0,900,50);
-        textField.setBackground(new Color(100,25,100));
+        textField.setBounds(0, 0, 900, 50);
+        textField.setBackground(new Color(100, 25, 100));
         textField.setForeground(Color.green);
-        textField.setFont(new Font("Ubuntu", Font.BOLD,25));
+        textField.setFont(new Font("Ubuntu", Font.BOLD, 25));
         textField.setBorder(BorderFactory.createBevelBorder(1));
         textField.setHorizontalAlignment(JTextField.CENTER);
         textField.setEditable(false);
 
-        textArea.setBounds(0,50,900,50);
+        textArea.setBounds(0, 50, 900, 50);
         textArea.setLineWrap(true);
         textArea.setWrapStyleWord(true);
-        textArea.setBackground(new Color(20,200,100));
+        textArea.setBackground(new Color(20, 200, 100));
         textArea.setForeground(Color.blue);
-        textArea.setFont(new Font("Ubuntu", Font.BOLD,35));
+        textArea.setFont(new Font("Ubuntu", Font.BOLD, 35));
         textArea.setBorder(BorderFactory.createBevelBorder(1));
         textArea.setEditable(false);
 
-        buttonA.setBounds(0,100,100,100);
-        buttonA.setFont(new Font("Ubuntu",Font.BOLD,35));
+        buttonA.setBounds(0, 100, 100, 100);
+        buttonA.setFont(new Font("Ubuntu", Font.BOLD, 35));
         buttonA.setFocusable(false);
         buttonA.addActionListener(this);
         buttonA.setText("A");
 
-        buttonB.setBounds(0,200,100,100);
-        buttonB.setFont(new Font("Ubuntu",Font.BOLD,35));
+        buttonB.setBounds(0, 200, 100, 100);
+        buttonB.setFont(new Font("Ubuntu", Font.BOLD, 35));
         buttonB.setFocusable(false);
         buttonB.addActionListener(this);
         buttonB.setText("B");
 
-        buttonC.setBounds(0,300,100,100);
-        buttonC.setFont(new Font("Ubuntu",Font.BOLD,35));
+        buttonC.setBounds(0, 300, 100, 100);
+        buttonC.setFont(new Font("Ubuntu", Font.BOLD, 35));
         buttonC.setFocusable(false);
         buttonC.addActionListener(this);
         buttonC.setText("C");
 
-        buttonD.setBounds(0,400,100,100);
-        buttonD.setFont(new Font("Ubuntu",Font.BOLD,35));
+        buttonD.setBounds(0, 400, 100, 100);
+        buttonD.setFont(new Font("Ubuntu", Font.BOLD, 35));
         buttonD.setFocusable(false);
         buttonD.addActionListener(this);
         buttonD.setText("D");
 
-        ans_labelA.setBounds(125,100,500,100);
-        ans_labelA.setBackground(new Color(15,50,130));
+        ans_labelA.setBounds(125, 100, 500, 100);
+        ans_labelA.setBackground(new Color(15, 50, 130));
         ans_labelA.setForeground(Color.green);
-        ans_labelA.setFont(new Font("Ubuntu",Font.PLAIN,20));
+        ans_labelA.setFont(new Font("Ubuntu", Font.PLAIN, 20));
 
-        ans_labelB.setBounds(125,200,500,100);
-        ans_labelB.setBackground(new Color(15,50,130));
+        ans_labelB.setBounds(125, 200, 500, 100);
+        ans_labelB.setBackground(new Color(15, 50, 130));
         ans_labelB.setForeground(Color.green);
-        ans_labelB.setFont(new Font("Ubuntu",Font.PLAIN,20));
+        ans_labelB.setFont(new Font("Ubuntu", Font.PLAIN, 20));
 
-        ans_labelC.setBounds(125,300,500,100);
-        ans_labelC.setBackground(new Color(15,50,130));
+        ans_labelC.setBounds(125, 300, 500, 100);
+        ans_labelC.setBackground(new Color(15, 50, 130));
         ans_labelC.setForeground(Color.green);
-        ans_labelC.setFont(new Font("Ubuntu",Font.PLAIN,20));
+        ans_labelC.setFont(new Font("Ubuntu", Font.PLAIN, 20));
 
-        ans_labelD.setBounds(125,400,500,100);
-        ans_labelD.setBackground(new Color(15,50,130));
+        ans_labelD.setBounds(125, 400, 500, 100);
+        ans_labelD.setBackground(new Color(15, 50, 130));
         ans_labelD.setForeground(Color.green);
-        ans_labelD.setFont(new Font("Ubuntu",Font.PLAIN,20));
+        ans_labelD.setFont(new Font("Ubuntu", Font.PLAIN, 20));
 
-        seconds_left.setBounds(800,512,100,100);
+        seconds_left.setBounds(800, 512, 100, 100);
         seconds_left.setBackground(Color.orange);
         seconds_left.setForeground(Color.red);
-        seconds_left.setFont(new Font("Ubuntu",Font.BOLD,45));
+        seconds_left.setFont(new Font("Ubuntu", Font.BOLD, 45));
         seconds_left.setBorder(BorderFactory.createBevelBorder(1));
         seconds_left.setOpaque(true);
         seconds_left.setHorizontalAlignment(JTextField.CENTER);
         seconds_left.setText(String.valueOf(seconds));
 
-        time_label.setBounds(800,477,100,25);
-        time_label.setBackground(new Color(50,50,50));
-        time_label.setForeground(new Color(255,160,30));
-        time_label.setFont(new Font("Ubuntu",Font.PLAIN,20));
+        time_label.setBounds(800, 477, 100, 25);
+        time_label.setBackground(new Color(50, 50, 50));
+        time_label.setForeground(new Color(255, 160, 30));
+        time_label.setFont(new Font("Ubuntu", Font.PLAIN, 20));
         time_label.setHorizontalAlignment(JTextField.CENTER);
         time_label.setText("TIMER ");
 
-        number_right.setBounds(400,225,200,100);
+        number_right.setBounds(400, 225, 200, 100);
         number_right.setBackground(Color.blue);
         number_right.setForeground(Color.green);
-        number_right.setFont(new Font("Ubuntu",Font.BOLD,25));
+        number_right.setFont(new Font("Ubuntu", Font.BOLD, 25));
         number_right.setBorder(BorderFactory.createBevelBorder(1));
         number_right.setHorizontalAlignment(JTextField.CENTER);
         number_right.setEditable(false);
 
-        percentage.setBounds(400,325,200,100);
+        percentage.setBounds(400, 325, 200, 100);
         percentage.setBackground(Color.LIGHT_GRAY);
         percentage.setForeground(Color.black);
-        percentage.setFont(new Font("Ubuntu",Font.BOLD,25));
+        percentage.setFont(new Font("Ubuntu", Font.BOLD, 25));
         percentage.setBorder(BorderFactory.createBevelBorder(1));
         percentage.setHorizontalAlignment(JTextField.CENTER);
         percentage.setEditable(false);
@@ -169,17 +184,18 @@ public class Quiz implements ActionListener {
 
     public void nextQuestion() {
 
-        if (index>=total_Questions){
+        if (index >= total_Questions) {
             results();
-        }
-        else {
-            textField.setText("Question "+(index)+1);
+        } else {
+            textField.setText("Question " + (index) + 1);
             textArea.setText(questions[index]);
             ans_labelA.setText(options[index][0]);
             ans_labelB.setText(options[index][1]);
             ans_labelC.setText(options[index][2]);
             ans_labelD.setText(options[index][3]);
         }
+
+        counter10.start();
 
     }
 
@@ -191,27 +207,27 @@ public class Quiz implements ActionListener {
         buttonC.setEnabled(false);
         buttonD.setEnabled(false);
 
-        if (e.getSource()==buttonA){
+        if (e.getSource() == buttonA) {
             answer = 'A';
-            if (answer == answers[index]){
+            if (answer == answers[index]) {
                 correct_guesses++;
             }
         }
-        if (e.getSource()==buttonB){
+        if (e.getSource() == buttonB) {
             answer = 'B';
-            if (answer == answers[index]){
+            if (answer == answers[index]) {
                 correct_guesses++;
             }
         }
-        if (e.getSource()==buttonC){
+        if (e.getSource() == buttonC) {
             answer = 'C';
-            if (answer == answers[index]){
+            if (answer == answers[index]) {
                 correct_guesses++;
             }
         }
-        if (e.getSource()==buttonD){
+        if (e.getSource() == buttonD) {
             answer = 'D';
-            if (answer == answers[index]){
+            if (answer == answers[index]) {
                 correct_guesses++;
             }
         }
@@ -219,22 +235,23 @@ public class Quiz implements ActionListener {
     }
 
     public void displayAnswer() {
+        counter10.stop();
 
         buttonA.setEnabled(false);
         buttonB.setEnabled(false);
         buttonC.setEnabled(false);
         buttonD.setEnabled(false);
 
-        if (answers[index]!= 'A'){
+        if (answers[index] != 'A') {
             ans_labelA.setForeground(Color.red);
         }
-        if (answers[index]!= 'B'){
+        if (answers[index] != 'B') {
             ans_labelB.setForeground(Color.red);
         }
-        if (answers[index]!= 'D'){
+        if (answers[index] != 'C') {
             ans_labelC.setForeground(Color.red);
         }
-        if (answers[index]!= 'D'){
+        if (answers[index] != 'D') {
             ans_labelD.setForeground(Color.red);
         }
 
@@ -246,7 +263,7 @@ public class Quiz implements ActionListener {
                 ans_labelC.setForeground(Color.green);
                 ans_labelD.setForeground(Color.green);
 
-                answer= ' ';
+                answer = ' ';
                 seconds = 10;
                 seconds_left.setText(String.valueOf(seconds));
                 buttonA.setEnabled(true);
@@ -258,10 +275,32 @@ public class Quiz implements ActionListener {
                 nextQuestion();
             }
         });
+        pause.setRepeats(false);
+        pause.start();
 
     }
 
     public void results() {
+
+        buttonA.setEnabled(false);
+        buttonB.setEnabled(false);
+        buttonC.setEnabled(false);
+        buttonD.setEnabled(false);
+
+        result = (int) ((correct_guesses / (double) total_Questions) * 100);
+        textField.setText("RESULT: ");
+        textArea.setText("");
+        ans_labelA.setText("");
+        ans_labelB.setText("");
+        ans_labelC.setText("");
+        ans_labelD.setText("");
+
+        number_right.setText("(" + correct_guesses + "/" + total_Questions + ")");
+        percentage.setText(result + "%");
+
+        frame.add(percentage);
+        frame.add(number_right);
+
 
     }
 }
