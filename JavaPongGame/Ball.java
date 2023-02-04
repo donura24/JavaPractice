23cbdbb435f5a31dcf46ae7ff2da5aab8f4a7b03
donura24 <teodorpenchev24@gmail.com>
2023-02-04 18:17:21 +0200

    commit

--- JavaPongGame/Ball.java
+++ JavaPongGame/Ball.java
@@ -8,7 +8,20 @@ public class Ball extends Rectangle{
     Random random;
     int xVelocity;
     int yVelocity;
-    Ball(){
+    Ball(int x, int y, int width, int height){
+        super(x,y,width,height);
+        random = new Random();
+        int randomXDirection = random.nextInt(2);
+        if (randomXDirection==0){
+            randomXDirection--;
+            setXDirection(randomXDirection);
+        }
+        random = new Random();
+        int randomYDirection = random.nextInt(2);
+        if (randomYDirection==0){
+            randomYDirection--;
+            setYDirection(randomYDirection);
+        }
 
     }
 
