d123f6cece7f29d04a8204387750461a59d52719
donura24 <teodorpenchev24@gmail.com>
2023-02-09 15:27:44 +0200

    commit

---
+++ DataStructuresAndAlgorithms/Queue/BubbleSort.java
@@ -0,0 +1,30 @@
+package JavaPractice.DataStructuresAndAlgorithms.Queue;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+
+        int[] array = {4, 6, 3, 9, 0, 7, 1, 2, 8, 5};
+
+        bubbleSort(array);
+
+        for (int i : array) {
+            System.out.print(i);
+
+        }
+    }
+
+    private static void bubbleSort(int array[]) {
+
+        for (int i = 0; i < array.length - 1; i++) {
+            for (int j = 0; j < array.length - i - 1; j++) {
+                if (array[j] > array[j + 1]) {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+
+            }
+
+        }
+    }
+}
