eea22a89fe4a6e4c0cfe39c10eb6571a3051260c
donura24 <teodorpenchev24@gmail.com>
2023-02-14 16:46:58 +0200

    commit

--- FibonacciWithRecursion/Main.java
+++ FibonacciWithRecursion/Main.java
@@ -1,16 +1,28 @@
 package JavaPractice.FibonacciWithRecursion;
 
 public class Main {
+    private static long[] fibonacciCache; // memoization
+
     public static void main(String[] args) {
 
 
         int n = 9;
 
-        System.out.println(fibonacci(n));
+        fibonacciCache = new long[n + 1];
+        for (int i = 0; i <=n ; i++) {
+            System.out.print(fibonacci(i)+" ");
+
+        }
     }
 
     private static long fibonacci(int n) {
         if (n <= 1) return n;
-        return (fibonacci(n - 1) + fibonacci(n - 2));
+
+        if (fibonacciCache[n] != 0) {
+            return fibonacciCache[n];
+        }
+        long nthFibonacciNumber = (fibonacci(n - 1) + fibonacci(n - 2));
+        fibonacciCache[n] = nthFibonacciNumber;
+        return nthFibonacciNumber;
     }
 }
