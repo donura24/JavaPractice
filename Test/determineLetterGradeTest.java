3e12c6eed140216c4c3739b9bdfd919f4fc1b6b6
donura24 <teodorpenchev24@gmail.com>
2023-02-19 23:01:12 +0200

    commit

--- Test/determineLetterGradeTest.java
+++ Test/determineLetterGradeTest.java
@@ -7,34 +7,82 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class determineLetterGradeTest {
     @Test
-    void fiftyNineEQ_F(){
+    void fiftyNineEQ_F() {
         var grade = new Grader();
         assertEquals('F', grade.determineLetterGrade(59));
 
     }
+
     @Test
-    void sixtyNineEQ_D(){
+    void sixtyNineEQ_D() {
         var grade = new Grader();
         assertEquals('D', grade.determineLetterGrade(69));
 
     }
+
     @Test
-    void seventyNineEQ_F(){
+    void seventyNineEQ_C() {
         var grade = new Grader();
         assertEquals('C', grade.determineLetterGrade(79));
 
     }
+
     @Test
-    void eightyNineEQ_F(){
+    void eightyNineEQ_B() {
         var grade = new Grader();
         assertEquals('B', grade.determineLetterGrade(89));
 
     }
+
     @Test
-    void ninetyNineEQ_F(){
+    void ninetyNineEQ_A() {
         var grade = new Grader();
         assertEquals('A', grade.determineLetterGrade(99));
 
+    }
+
+    @Test
+    void eightyEQ_B() {
+        var grade = new Grader();
+        assertEquals('B', grade.determineLetterGrade(80));
+
+    }
+
+    @Test
+    void ninetyEQ_A() {
+        var grade = new Grader();
+        assertEquals('A', grade.determineLetterGrade(90));
+
+    }
+
+    @Test
+    void seventyEQ_C() {
+        var grade = new Grader();
+        assertEquals('C', grade.determineLetterGrade(70));
+
+    }
+
+    @Test
+    void sixtyEQ_D() {
+        var grade = new Grader();
+        assertEquals('D', grade.determineLetterGrade(60));
+
+    }
+
+    @Test
+    void zeroEQ_F() {
+        var grade = new Grader();
+        assertEquals('F', grade.determineLetterGrade(0));
+
+    }
+
+    @Test
+    void negOneShouldReturnIllegalArgumentException() {
+        var grade = new Grader();
+        assertThrows(IllegalArgumentException.class,
+                () -> {
+                    grade.determineLetterGrade(-1);
+                });
     }
 
 }
