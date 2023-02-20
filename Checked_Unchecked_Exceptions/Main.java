696c1055e6e3396f7d49c769de61dbf3388a1e9b
donura24 <teodorpenchev24@gmail.com>
2023-02-20 22:31:42 +0200

    commit

---
+++ Checked_Unchecked_Exceptions/Main.java
@@ -0,0 +1,18 @@
+package JavaPractice.Checked_Unchecked_Exceptions;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            readFile("file1.txt");
+        } catch (FileNotFoundException e) {
+            System.out.println("File doesn't exist!");
+        }
+    }
+    private  static void readFile(String fileName) throws FileNotFoundException {
+        FileReader reader = new FileReader(fileName);
+    }
+
+}
