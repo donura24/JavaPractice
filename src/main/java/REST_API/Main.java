5a23b46abeac175f4df89d7b3935ce6eb3a88433
donura24 <teodorpenchev24@gmail.com>
2023-02-23 16:28:40 +0200

    M1

---
+++ src/main/java/REST_API/Main.java
@@ -0,0 +1,35 @@
+package REST_API;
+
+import com.google.gson.Gson;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class Main {
+    public static void main(String[] args) throws URISyntaxException, IOException, InterruptedException {
+
+        Transcript transcript = new Transcript();
+        transcript.setAudio_url("https://github.com/donura24/JavaPractice/raw/main/240895.wav");
+
+        Gson gson = new Gson();
+        String jsonReq = gson.toJson(transcript);
+        System.out.println(jsonReq);
+
+        HttpRequest postRequest = HttpRequest.newBuilder()
+                .uri(new URI("https://api.assemblyai.com/v2/transcript"))
+                .header("Authorization", "b244a137272e401289ac45959b940c78")
+                .POST(HttpRequest.BodyPublishers.ofString(jsonReq))
+                .build();
+
+        HttpClient httpClient = HttpClient.newHttpClient();
+        HttpResponse<String> postResponse = httpClient.send(postRequest, HttpResponse.BodyHandlers.ofString());
+
+        System.out.println(postResponse.body());
+
+
+    }
+}
