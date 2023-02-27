185d6dd724e450f33754304860bdc18871f45bc0
donura24 <teodorpenchev24@gmail.com>
2023-02-27 18:48:44 +0200

    commit

---
+++ Map_HashMap/Map_s.java
@@ -0,0 +1,22 @@
+package JavaPractice.Map_HashMap;
+
+import java.util.HashMap;
+
+public class Map_s {
+    public static void main(String[] args) {
+
+        HashMap<String, Integer> empIDs = new HashMap<>();
+
+        empIDs.put("Teodor", 45829);
+        empIDs.put("Andrey", 45123);
+        empIDs.put("Tea", 45689);
+        //System.out.println(empIDs);
+        System.out.println(empIDs.get("Tea"));
+
+        System.out.println(empIDs.containsKey("Teodor"));
+
+        System.out.println(empIDs.containsValue(45123));
+
+        empIDs.put("Teodor",12345); // will update and overwrite the current value
+    }
+}
