af0397e192a4ba794dc14f5f0c20c4f49ce700f0
donura24 <teodorpenchev24@gmail.com>
2023-02-27 19:14:53 +0200

    commit

--- Map_HashMap/Map_s.java
+++ Map_HashMap/Map_s.java
@@ -10,7 +10,7 @@ public class Map_s {
         empIDs.put("Teodor", 45829);
         empIDs.put("Andrey", 45123);
         empIDs.put("Tea", 45689);
-        //System.out.println(empIDs);
+
         System.out.println(empIDs.get("Tea"));
 
         System.out.println(empIDs.containsKey("Teodor"));
@@ -18,5 +18,10 @@ public class Map_s {
         System.out.println(empIDs.containsValue(45123));
 
         empIDs.put("Teodor",12345); // will update and overwrite the current value
+        empIDs.replace("John",23555);
+        empIDs.putIfAbsent("John",23458);
+        empIDs.remove("John");
+
+        System.out.println(empIDs);
     }
 }
