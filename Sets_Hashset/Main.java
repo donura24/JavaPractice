5f1296a08b45318edc807d5b1da9f0751c3e66dd
donura24 <teodorpenchev24@gmail.com>
2023-02-28 18:42:40 +0200

    commit

--- Sets_Hashset/Main.java
+++ Sets_Hashset/Main.java
@@ -1,7 +1,6 @@
 package JavaPractice.Sets_Hashset;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -13,7 +12,30 @@ public class Main {
         names.add("Andrey");
         //names.add("Teodor"); // Sets do not allow duplicates
         names.remove("Teodor");
+        //names.clear();
 
-        System.out.println(names.size());
+
+        //System.out.println(names.contains("Teodor"));
+
+//        for (String name : names){
+//            System.out.println(name);
+//        }
+        //names.forEach(System.out::println);
+
+        Iterator<String> namesIterator = names.iterator();
+        while (namesIterator.hasNext()){
+            System.out.println(namesIterator.next());
+        }
+// Using sets -
+        List<Integer> numberList = new ArrayList<>();
+        numberList.add(1);
+        numberList.add(3);
+        numberList.add(2);
+        numberList.add(1);
+        System.out.println(numberList);
+
+        Set<Integer> numSet = new HashSet<>();
+        numSet.addAll(numberList);
+        System.out.println(numSet);
     }
 }
