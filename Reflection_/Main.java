38cd5e82eecfefd6fe1053ff7626e5165c466557
donura24 <teodorpenchev24@gmail.com>
2023-03-01 21:19:28 +0200

    commit

--- Reflection_/Main.java
+++ Reflection_/Main.java
@@ -1,7 +1,30 @@
 package JavaPractice.Reflection_;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IllegalAccessException, InvocationTargetException {
+
+        Snake snake1 = new Snake("Snakea",2);
+        Field[] snakefields = snake1.getClass().getDeclaredFields();
+
+        for (Field field : snakefields){
+            if (field.getName().equals("name")){
+                field.setAccessible(true);
+                field.set(snake1,"SnakeNewName");
+            }
+        }
+        Method [] snakeMethods = snake1.getClass().getDeclaredMethods();
+        for (Method method : snakeMethods){
+            if (method.getName().equals("thisIsPrivate")){
+                method.setAccessible(true);
+                method.invoke(snake1);
+            }
+            System.out.println(method.getName());
+        }
+        System.out.println(snake1.getName());
 
     }
 }
