e476bf3e92e2f96337ea634c2a52b440822466a8
donura24 <teodorpenchev24@gmail.com>
2023-03-03 12:50:39 +0200

    commit

--- FunctionalProgramming/Imperative/Main.java
+++ FunctionalProgramming/Imperative/Main.java
@@ -1,15 +1,35 @@
 package JavaPractice.FunctionalProgramming.Imperative;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+
+import static JavaPractice.FunctionalProgramming.Imperative.Main.Gender.FEMALE;
 
 public class Main {
     public static void main(String[] args) {
         List<Person> people = List.of(
                 new Person("Teodor", Gender.MALE),
-                new Person("Stefany", Gender.FEMALE),
+                new Person("Stefany", FEMALE),
                 new Person("Andrey", Gender.MALE),
-                new Person("Ivana", Gender.FEMALE)
+                new Person("Ivana", FEMALE)
         );
+
+        System.out.println("Imperative approach"); // Imperative approach
+        List<Person> females = new ArrayList<>();
+        for (Person person : people){
+            if (FEMALE.equals(person.gender)){
+                females.add(person);
+                System.out.println(person);
+            }
+        }
+
+        System.out.println("Declarative approach"); //Declarative approach
+        people.stream()
+                .filter(person -> FEMALE.equals(person.gender))
+                .collect(Collectors.toList())
+                .forEach(System.out::println);
+
     }
 
     static class Person {
