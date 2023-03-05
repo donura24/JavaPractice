ef6ade4e36972c524c771b7bbc706cad80a05b94
donura24 <teodorpenchev24@gmail.com>
2023-03-05 11:01:18 +0200

    commit

--- FunctionalProgramming/FunctionalInterface/_Consumer.java
+++ FunctionalProgramming/FunctionalInterface/_Consumer.java
@@ -1,28 +1,35 @@
 package JavaPractice.FunctionalProgramming.FunctionalInterface;
 
+import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 
 public class _Consumer {
     public static void main(String[] args) {
-        Customer cus1 = new Customer("Teo","088888812");
+        Customer cus1 = new Customer("Teo", "088888812");
         // Normal Java function
         // greetCustomer(cus1);
 
         // Consumer functional interface
         greetCustomerConsumer.accept(cus1);
+        greetCustomerBiConsumer.accept(cus1,false);
     }
 
+    static BiConsumer<Customer, Boolean> greetCustomerBiConsumer = (customer, showPN) ->
+            System.out.println("Hello " + customer.customerName
+                    + ", you registered phone number: "
+                    + (showPN ? customer.customerPhoneNumber : "**********"));
     static Consumer<Customer> greetCustomerConsumer = customer ->
-            System.out.println("Hello "+customer.customerName
-                    +", you registered phone number: "
-                    +customer.customerPhoneNumber);
+            System.out.println("Hello " + customer.customerName
+                    + ", you registered phone number: "
+                    + customer.customerPhoneNumber);
 
-    static void greetCustomer(Customer customer){
-        System.out.println("Hello "+customer.customerName
-                +", you registered phone number: "
-                +customer.customerPhoneNumber);
+    static void greetCustomer(Customer customer) {
+        System.out.println("Hello " + customer.customerName
+                + ", you registered phone number: "
+                + customer.customerPhoneNumber);
     }
-    static class Customer{
+
+    static class Customer {
         private final String customerName;
         private final String customerPhoneNumber;
 
