ca2f8f06412b4be0227124b29277f08874140eb3
donura24 <teodorpenchev24@gmail.com>
2023-03-05 11:23:55 +0200

    commit

--- FunctionalProgramming/FunctionalInterface/_Predicate.java
+++ FunctionalProgramming/FunctionalInterface/_Predicate.java
@@ -4,10 +4,14 @@ import java.util.function.Predicate;
 
 public class _Predicate {
     public static void main(String[] args) {
+        String phoneNumber1 = "07433345699";
         System.out.println(isPhoneNumberValid("08543323"));
 
         // Using Predicate
         System.out.println(isPhoneNumberValidPredicate.test("07943569691"));
+
+        System.out.println("Phone number: " + phoneNumber1 + " valid and contains number 3: "
+                + isPhoneNumberValidPredicate.and(containsNumber3).test(phoneNumber1));
     }
 
     static boolean isPhoneNumberValid(String phoneNumber) {
@@ -16,4 +20,7 @@ public class _Predicate {
 
     static Predicate<String> isPhoneNumberValidPredicate = phoneNumber ->
             phoneNumber.startsWith("07") && phoneNumber.length() == 11;
+
+    static Predicate<String> containsNumber3 = phoneNumber ->
+            phoneNumber.contains("3");
 }
