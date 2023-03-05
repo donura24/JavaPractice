b14b50775985bf5e1be39e1a38d0d05f5a3e5803
donura24 <teodorpenchev24@gmail.com>
2023-03-05 10:51:06 +0200

    commit

---
+++ FunctionalProgramming/FunctionalInterface/_Consumer.java
@@ -0,0 +1,35 @@
+package JavaPractice.FunctionalProgramming.FunctionalInterface;
+
+import java.util.function.Consumer;
+
+public class _Consumer {
+    public static void main(String[] args) {
+        Customer cus1 = new Customer("Teo","088888812");
+        // Normal Java function
+        // greetCustomer(cus1);
+
+        // Consumer functional interface
+        greetCustomerConsumer.accept(cus1);
+    }
+
+    static Consumer<Customer> greetCustomerConsumer = customer ->
+            System.out.println("Hello "+customer.customerName
+                    +", you registered phone number: "
+                    +customer.customerPhoneNumber);
+
+    static void greetCustomer(Customer customer){
+        System.out.println("Hello "+customer.customerName
+                +", you registered phone number: "
+                +customer.customerPhoneNumber);
+    }
+    static class Customer{
+        private final String customerName;
+        private final String customerPhoneNumber;
+
+        Customer(String customerName, String customerPhoneNumber) {
+            this.customerName = customerName;
+            this.customerPhoneNumber = customerPhoneNumber;
+        }
+    }
+
+}
