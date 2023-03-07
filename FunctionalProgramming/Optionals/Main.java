5e84623fdfc31c11c0a32e55734c23704be140ad
donura24 <teodorpenchev24@gmail.com>
2023-03-07 11:39:11 +0200

    commit

---
+++ FunctionalProgramming/Optionals/Main.java
@@ -0,0 +1,18 @@
+package JavaPractice.FunctionalProgramming.Optionals;
+
+import java.util.Optional;
+import java.util.function.Supplier;
+
+public class Main {
+    public static void main(String[] args) {
+        Supplier<IllegalArgumentException> exceptionSupplier = () -> new IllegalArgumentException("exception");
+
+//        Object o = Optional.ofNullable(null)
+//                .orElseThrow(exceptionSupplier);
+
+        Optional.ofNullable(null)
+                //.ifPresent(name -> System.out.println("Your name is: " + name));
+                .ifPresentOrElse(name -> System.out.println("Your name is: " + name),
+                        () -> System.out.println("Please enter a name.") );
+    }
+}
