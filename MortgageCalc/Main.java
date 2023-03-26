c10655ad05f83e0a9c8b9dc571a0997e6bb0931d
donura24 <teodorpenchev24@gmail.com>
2023-03-26 22:10:20 +0300

    commit

--- MortgageCalc/Main.java
+++ MortgageCalc/Main.java
@@ -18,13 +18,15 @@ public class Main {
 
         System.out.println("Enter the term in years: ");
         int termInYears = scanner.nextInt();
+        scanner.close();
 
         float monthlyInterestRate = annualInterestRate / MONTHS_IN_YEAR;
         int numberOfPayments = termInYears * MONTHS_IN_YEAR;
 
         double monthlyPayment = principal * (
-                monthlyInterestRate * ((Math.pow(1+ monthlyInterestRate, numberOfPayments))
-                ) / ((Math.pow(1+ monthlyInterestRate, numberOfPayments)) -1 ));
+                monthlyInterestRate * ((Math.pow(1 + monthlyInterestRate, numberOfPayments))
+                ) / ((Math.pow(1 + monthlyInterestRate, numberOfPayments)) - 1));
         System.out.println("monthly payment: " + NumberFormat.getCurrencyInstance().format(monthlyPayment));
+        System.out.println("Total payback amount: " + NumberFormat.getCurrencyInstance().format(numberOfPayments * monthlyPayment));
     }
 }
