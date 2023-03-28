08cc7c5690c9253989892466e21b062cbba80b38
donura24 <teodorpenchev24@gmail.com>
2023-03-28 21:43:45 +0300

    commit

---
+++ Encapsulation/Employee.java
@@ -0,0 +1,32 @@
+package JavaPractice.Encapsulation;
+
+public class Employee {
+
+    String name;
+    int age;
+    int workExp;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getWorkExp() {
+        return workExp;
+    }
+
+    public void setWorkExp(int workExp) {
+        this.workExp = workExp;
+    }
+}
---
+++ Encapsulation/Main.java
@@ -0,0 +1,7 @@
+package JavaPractice.Encapsulation;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
