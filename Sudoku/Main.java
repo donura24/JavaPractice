59aca2068e8897e8f0a4c26c476ffe2703ce9dd0
donura24 <teodorpenchev24@gmail.com>
2023-03-30 23:17:07 +0300

    commit

--- Sudoku/Main.java
+++ Sudoku/Main.java
@@ -2,7 +2,7 @@ package JavaPractice.Sudoku;
 
 public class Main {
 
-    private static final int GRID_SIZE=9;
+    private static final int GRID_SIZE = 9;
 
     public static void main(String[] args) {
 
@@ -19,20 +19,42 @@ public class Main {
                 {0, 0, 7, 0, 4, 0, 2, 0, 3}
         };
     }
-    private static boolean isNumberInRow(int[][] board, int number, int row){
+
+    private static boolean isNumberInRow(int[][] board, int number, int row) {
         for (int i = 0; i < GRID_SIZE; i++) {
-            if (board[row][i] == number){
+            if (board[row][i] == number) {
                 return true;
             }
         }
         return false;
     }
-    private static boolean isNumberInColumn(int[][] board, int number, int column){
+
+    private static boolean isNumberInColumn(int[][] board, int number, int column) {
         for (int i = 0; i < GRID_SIZE; i++) {
-            if (board[i][column] == number){
+            if (board[i][column] == number) {
                 return true;
             }
         }
         return false;
+    }
+
+    private static boolean isNumberInBox(int[][] board, int number, int row, int column) {
+        int localBoxRow = row - row % 3;
+        int localBoxColumn = column - column % 3;
+
+        for (int i = localBoxRow; i < localBoxRow + 3; i++) {
+            for (int j = localBoxColumn; j < localBoxColumn + 3; j++) {
+                if (board[i][j] == number) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private static boolean isValidPlacement(int[][] board, int number, int row, int column) {
+        return !isNumberInBox(board, number, row, column) &&
+                !isNumberInRow(board, number, row) &&
+                !isNumberInColumn(board, number, column);
     }
 }
