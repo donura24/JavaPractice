74ab6b3009f9d7f762fb48b85604a6895df026ca
donura24 <teodorpenchev24@gmail.com>
2023-03-30 23:40:10 +0300

    commit

--- Sudoku/Main.java
+++ Sudoku/Main.java
@@ -18,7 +18,34 @@ public class Main {
                 {0, 0, 0, 2, 0, 0, 0, 0, 0},
                 {0, 0, 7, 0, 4, 0, 2, 0, 3}
         };
+
+        if (solveboard(board)){
+            System.out.println("Solved!");
+        }
+        else {
+            System.out.println("Unsolvable board!");
+        }
+        printBoard(board);
     }
+
+    private static void printBoard(int[][] board) {
+        for (int row = 0; row < GRID_SIZE; row++) {
+            if (row % 3 == 0 && row !=0){
+
+                System.out.println("-------");
+            }
+            for (int column = 0; column < GRID_SIZE; column++) {
+                if (column % 3 ==0 && column != 0 ){
+                    System.out.print("|");
+                }
+                System.out.print(board[row][column]);
+            }
+                System.out.println();
+
+            }
+
+        }
+
 
     private static boolean isNumberInRow(int[][] board, int number, int row) {
         for (int i = 0; i < GRID_SIZE; i++) {
