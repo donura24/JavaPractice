5539d133033c66e75581f65c19b4cd18e9c1ad64
donura24 <teodorpenchev24@gmail.com>
2023-04-01 18:19:24 +0300

    fc

--- JavaLoginGUI/Main.java
+++ JavaLoginGUI/Main.java
@@ -4,7 +4,7 @@ import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class Main  implements ActionListener {
+public class Main implements ActionListener {
 
     private static JFrame frame;
     private static JPanel panel;
@@ -17,51 +17,40 @@ public class Main  implements ActionListener {
 
     public static void main(String[] args) {
 
-
-
+        panel = new JPanel();
 
         frame = new JFrame("My APP");
-        frame.setSize(300,180);
+        frame.setSize(300, 180);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLocationRelativeTo(null);
         frame.add(panel);
 
-        panel = new JPanel();
         panel.setLayout(null);
 
-
         userlabel = new JLabel("User:");
-        userlabel.setBounds(10,20,80,25);
+        userlabel.setBounds(10, 20, 80, 25);
         panel.add(userlabel);
 
-
         passlabel = new JLabel("Password:");
-        passlabel.setBounds(10,50,80,25);
+        passlabel.setBounds(10, 50, 80, 25);
         panel.add(passlabel);
 
-
-
         userField = new JTextField();
-        userField.setBounds(100, 20,165,25);
+        userField.setBounds(100, 20, 165, 25);
         panel.add(userField);
 
-
         passField = new JPasswordField();
-        passField.setBounds(100, 50,165,25);
+        passField.setBounds(100, 50, 165, 25);
         panel.add(passField);
 
-
         button = new JButton("LOGIN");
-        button.setBounds(125,90 , 80, 25);
+        button.setBounds(125, 90, 80, 25);
         button.addActionListener(new Main());
         panel.add(button);
 
-
         success = new JLabel("");
-        success.setBounds(10,110,300,25);
+        success.setBounds(10, 110, 300, 25);
         panel.add(success);
-
-
 
         frame.setVisible(true);
 
@@ -72,5 +61,9 @@ public class Main  implements ActionListener {
     public void actionPerformed(ActionEvent e) {
         String user = userField.getText();
         String pass = passField.getText();
+
+        if (user.equals("Teodor")&& pass.equals("123")){
+            success.setText("Login successful!");
+        }
     }
 }
