3d564df42216d361f3a094b76e30e1a777abc8c1
donura24 <teodorpenchev24@gmail.com>
2023-04-05 17:40:28 +0300

    nc

---
+++ BMI_Calculator/Main.java
@@ -0,0 +1,20 @@
+package JavaPractice.BMI_Calculator;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter weight in pounds: ");
+        double weight  = input.nextDouble();
+
+        System.out.println("Enter height in inches: ");
+        double height = input.nextDouble();
+
+        double bmi = weight / Math.pow(height, 2) * 703;
+
+        System.out.printf("Your BMI is:"+ bmi);
+    }
+}
---
+++ target/classes/README.md
@@ -0,0 +1,4 @@
+# Java Practice 
+
+#### Repository where I train and maintain my Java programming skills.
+Nothing special, just the fundamentals of programming.
